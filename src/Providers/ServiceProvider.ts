a3c2a52ca9860a5b43a31f727661d06d76c83463
yuna611t <yuna611t@gmail.com>
2019-04-20 21:49:39 +0900

    Finish Prototype Implementation

---
+++ src/Providers/ServiceProvider.ts
@@ -0,0 +1,20 @@
+import { Service, Encoding } from "../Services/Service";
+
+export class ServiceProvider {
+    public provide(pattern: ServiceType): Service {
+        if (pattern === ServiceType.SJIStoUTF8) {
+            return new Service({srcEncoding: Encoding.Shift_JIS, distEncoding: Encoding.UTF8});
+        }
+        if (pattern === ServiceType.UTF8toSJIS) {
+            return new Service({srcEncoding: Encoding.UTF8, distEncoding: Encoding.Shift_JIS});
+        }
+        return new Service({srcEncoding: Encoding.Shift_JIS, distEncoding: Encoding.UTF8});
+    }
+}
+
+
+
+export enum ServiceType {
+    "SJIStoUTF8",
+    "UTF8toSJIS",
+}
